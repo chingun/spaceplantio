864d8bf59f6214544357e34faeb86ed3cb7d1835
chingun <chingun@umich.edu>
2022-02-13 20:05:19 -0500

    Done Diddo

--- src/component/me.component.js
+++ src/component/me.component.js
@@ -7,6 +7,7 @@ import heart from './circuit/retroheart.png';
 import circuit from './circuit/retroboardclean.png';
 import kick from './kickstarter/kicklogo.png';
 import beacon from './beacon/beacon.png';
+import beacon_vert from './beacon/beacon_vert.png';
 import logo from './logo/AppLogo256.png';
 import { AiFillGithub, AiFillInstagram, AiFillMail } from "react-icons/ai";
 
@@ -17,13 +18,31 @@ class Me extends Component {
 
         this.state = {
             location: 'about', // can be ABOUT || EMBEDDED || COMPSCI || ML
-            items: []
+            items: [],
+            width: 0, 
+            height: 0
         }
+
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     }
 
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, 
+                        height: window.innerHeight });
+    }
+
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+      
     about(){
         return(
-            <div className="body">
+            <div className="main_body">
                 <p> 
                     Space Plant aims to accelerate terraforming of terrestrial planets through botany innovations. 
                 </p>
@@ -50,11 +69,9 @@ class Me extends Component {
             return(<></>)
         }
           return(
-            <div className="body">
-                <p> 
+            <p className="main_body">
                 We constantly improve our app based on user requests. Currently we only support iOS [plans to support Android based on demand].
-                </p>
-            </div>
+            </p>
         );
     }
       
@@ -63,25 +80,29 @@ class Me extends Component {
             return(<></>)
         }
         return(
-            <div className="body">
-                <p> 
+            <p className="main_body">
                 We use cutting edge deep neural networks [Transformer + CNN] to ensure the best possible results. We estimate that in 94% of cases, we offer the right answer.
-                </p>
-            </div>
+            </p>
         );
     }
 
     sensor(){
         if (this.state.location !== "sensor"){
-            return(<></>)
+            return(<></>) 
         }
-        return(
-            // <div className="body">
-            <p style={{"margin-left": "auto", "margin-right": "auto"}}> 
-                <img src={beacon} width="1000"/>
-            </p>
-            // </div>
-        );
+        if(this.state.width > 800){
+            return( 
+                <p style={{"margin-left": "auto", "margin-right": "auto"}}> 
+                    <img src={beacon} width={window.innerWidth * 0.8}/>
+                </p> 
+            );
+        }
+        else {
+            return(  
+                <img src={beacon_vert} width={this.state.width}/> 
+            );
+        }
+
     }
 
 
@@ -96,22 +117,17 @@ class Me extends Component {
     }
 
     render(){
+        
         return(
-        <div className="main-container" style={this.state.location === "sensor" ? {backgroundColor: "black"}: {backgroundColor: "white"}}>
+        <div className="main-container" style={this.state.location === "sensor" ? {height: "100%", backgroundColor: "black"}: {backgroundColor: "white"}}>
             <div className="navbar">
-                <p className="navbar-image">
-                    <img src={logo} width="50px"/>
-                </p>
-                <h1>  
-                    <a className="h1-logo" style={this.state.location === "sensor" ? {color: "white"}:{}} onClick={() => {this.setState({location: 'about'})}}> space plant </a>
-                </h1>
-                <p className="navbar-links">
-                    <a onClick={() => {this.setState({location: 'ai'})}}> ai </a>
-                    <a onClick={() => {this.setState({location: 'app'})}}> app </a>
-                    <a onClick={() => {this.setState({location: 'sensor'})}}> sensor </a>
-                    {/* <a href="mailto:chingun@umich.edu"> email </a>
-                    <a href="https://github.com/chingun"> git </a> */}
-                </p>
+                <div className="navbar-image-container"><img className="navbar-image" src={logo}/></div>
+                <h1><a className="h1-logo"  style={this.state.location === "sensor" ? {color: "white"}:{}} onClick={() => {this.setState({location: 'about'})}}>space plant</a></h1>
+                <div className="navbar-links">
+                    <a onClick={() => {this.setState({location: 'ai'})}}>ai</a>
+                    <a onClick={() => {this.setState({location: 'app'})}}>app</a>
+                    <a onClick={() => {this.setState({location: 'sensor'})}}>sensor</a>
+                </div>
             </div>
             {this.state.location === "app" ? this.app() : this.state.location === "ai" ? this.ai() : this.state.location === "sensor" ? this.sensor() : this.about()}
             <br/>
